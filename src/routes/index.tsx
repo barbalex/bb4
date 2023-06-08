02fefa41c502d7f455ca098cb74aa14fa93d141a
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-08 18:09:01 +0200

    configuring

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from '@builder.io/qwik'
+import type { DocumentHead } from '@builder.io/qwik-city'
 
-import Counter from '~/components/starter/counter/counter';
-import Hero from '~/components/starter/hero/hero';
-import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
+import Counter from '~/components/starter/counter/counter'
+import Hero from '~/components/starter/hero/hero'
+import Infobox from '~/components/starter/infobox/infobox'
+import Starter from '~/components/starter/next-steps/next-steps'
 
 export default component$(() => {
   return (
@@ -98,15 +98,16 @@ export default component$(() => {
         </div>
       </div>
     </>
-  );
-});
+  )
+})
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'mediterranean migration',
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: 'mediterranean migration',
+      content:
+        'Overview over migration across the the blue borders of the Me­diterrane­an',
     },
   ],
-};
+}
