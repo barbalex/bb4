be9ba501d606804a192fe3e8884b721150b0de6d
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-14 21:29:48 +0200

    style

--- src/components/events/header.tsx
+++ src/components/events/header.tsx
@@ -1,8 +1,8 @@
 import { component$ } from '@builder.io/qwik'
 
 export default component$(() => (
-  <div class="sticky top-14 mt-6 w-full pb-1.5 font-bold text-2xl break-words bg-white z-10 border-solid border border-white border-b-slate-200">
-    <div class="flex w-full">
+  <div class="sticky top-14 mt-6 pb-1.5 font-bold text-2xl break-words bg-white z-10 border-solid border border-white border-b-slate-200">
+    <div class="flex">
       <div class="grow-0 shrink-0 basis-14 pr-4 text-right"></div>
       <div class="grow-1 shrink-1 basis-1/2 pr-2 text-center">
         Maritime Events
--- src/components/events/statisticRow.tsx
+++ src/components/events/statisticRow.tsx
@@ -5,14 +5,14 @@ import Event from './event'
 export default component$(({ data }) => (
   <div class="flex border-t border-solid border-slate-200 bg-slate-100 hover:bg-slate-200">
     <p class="grow-0 shrink-0 basis-14 p-1.5 pr-5 text-right"></p>
-    <div class="grow-0 shrink-0 basis-1/2 pr-2.5 break-words">
+    <div class="grow-1 shrink-1 basis-1/2 pr-2.5 break-words">
       <ul class="px-4">
         {data.migrationEvents.map((event) => (
           <Event key={event.id} event={event} />
         ))}
       </ul>
     </div>
-    <div class="grow-0 shrink-0 basis-1/2 pr-2.5 break-words">
+    <div class="grow-1 shrink-1 basis-1/2 pr-2.5 break-words">
       <ul class="px-4">
         {data.politicEvents.map((event) => (
           <Event key={event.id} event={event} />
