61b35bd02224e5ed00461b1bbbd8400b00788aee
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-14 15:19:04 +0200

    work on events

--- src/components/events/event.tsx
+++ src/components/events/event.tsx
@@ -9,7 +9,7 @@ export default component$(({ event }) => {
     <li
       class={
         event.tags?.length
-          ? `${eventClasses} py-1.5 list-none pl-1.5`
+          ? `py-1.5 list-none pl-1.5`
           : `py-1.5 list-disc pl-1.5`
       }
     >
@@ -20,7 +20,7 @@ export default component$(({ event }) => {
             <span class="pl-1" key={key}>
               {key > 0 && ' '}
               <a
-                class="after:content-[url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==)]"
+                class="after:content-[url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==)] font-normal"
                 href={l.url}
                 target="_blank"
                 rel="noopener noreferrer"
