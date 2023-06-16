0650c170c72541678bad3187de38b2e5a6626271
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-16 16:48:01 +0200

    work on editing

--- src/components/events/event.tsx
+++ src/components/events/event.tsx
@@ -49,13 +49,36 @@ export default component$(({ event }) => {
             >
               <EditIcon class="text-sm" />
             </button>
-            <button
-              type="button"
-              class="rounded-full hover:bg-gray-200 ml-1 p-1"
-              data-title="delete"
-            >
-              <DeleteIcon class="text-red-600 font-bold text-sm" />
-            </button>
+            <div class="relative inline" aria-expanded="false">
+              <button
+                type="button"
+                class="rounded-full hover:bg-gray-200 ml-1 p-1"
+                data-title="delete"
+              >
+                <DeleteIcon class="text-red-600 font-bold text-sm" />
+              </button>
+              <div class="absolute left-1/2 z-10 flex -translate-x-1/2 px-4">
+                <div class="w-auto flex-auto overflow-hidden rounded-md p-2 pt-1 bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+                  <p class="font-semibold whitespace-nowrap">
+                    Delete this event?
+                  </p>
+                  <div class="mt-1 flex justify-between">
+                    <button
+                      type="button"
+                      class="rounded bg-red-600 px-4 py-2 text-xs font-bold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-red-800"
+                    >
+                      Yes
+                    </button>
+                    <button
+                      type="button"
+                      class="rounded bg-white px-4 py-2 text-xs font-bold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    >
+                      No
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </>
         )}
       </div>
