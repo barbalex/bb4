24bd8e307ede1ec59fe9f1d1400946278c2a9faa
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-16 16:00:54 +0200

    work on editing

--- src/components/events/event.tsx
+++ src/components/events/event.tsx
@@ -1,4 +1,8 @@
 import { component$, useContext } from '@builder.io/qwik'
+import {
+  BsPencilFill as EditIcon,
+  BsXCircle as DeleteIcon,
+} from '@qwikest/icons/bootstrap'
 
 import { CTX } from '~/root'
 
@@ -20,8 +24,8 @@ export default component$(({ event }) => {
           .map((tag) => `event-${tag}`)
           .join(' ')} relative`}
       >
-        {event.title}{' '}
-        <span>
+        {event.title}&nbsp;
+        <>
           {(event.links ?? []).map((l, key) => (
             <span class="pl-1" key={l.label}>
               {key > 0 && ' '}
@@ -35,7 +39,25 @@ export default component$(({ event }) => {
               </a>
             </span>
           ))}
-        </span>
+        </>
+        {showEditingGlyphons && (
+          <>
+            <button
+              type="button"
+              class="rounded-full hover:bg-gray-200 ml-2 p-1"
+              data-title="edit"
+            >
+              <EditIcon class="text-sm" />
+            </button>
+            <button
+              type="button"
+              class="rounded-full hover:bg-gray-200 ml-1 p-1"
+              data-title="delete"
+            >
+              <DeleteIcon class="text-red-600 font-bold text-sm" />
+            </button>
+          </>
+        )}
       </div>
     </li>
   )
