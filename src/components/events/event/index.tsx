7c4dec66b8b91661df28cafc482ccb6c43d2ffae
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-26 22:39:28 +0200

    modularise confirming deletions

--- src/components/events/event/index.tsx
+++ src/components/events/event/index.tsx
@@ -2,6 +2,7 @@ import {
   component$,
   useContext,
   useSignal,
+  $,
   // useVisibleTask$,
 } from '@builder.io/qwik'
 import { useNavigate, server$ } from '@builder.io/qwik-city'
@@ -12,6 +13,7 @@ import {
 
 import { CTX } from '~/root'
 import * as db from '~/db'
+import ConfirmDeletion from '~/components/shared/confirmDeletion'
 
 const deleter = server$(async function ({ id }) {
   try {
@@ -114,31 +116,14 @@ export default component$(({ event, refetcher }) => {
                     : 'transition duration-200 ease-out opacity-0 translate-y-1 h-0'
                 }`}
               >
-                <div class="w-auto flex-auto overflow-hidden rounded-md p-2 pt-1 bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-                  <p class="font-semibold whitespace-nowrap">
-                    Delete this event?
-                  </p>
-                  <div class="mt-1 flex justify-between">
-                    <button
-                      type="button"
-                      class="rounded bg-red-600 px-4 py-2 text-xs font-bold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:cursor-pointer hover:bg-red-800"
-                      onClick$={async () => {
-                        deleteMenuOpen.value = false
-                        await deleter({ id: event.id })
-                        refetcher.value++
-                      }}
-                    >
-                      Yes
-                    </button>
-                    <button
-                      type="button"
-                      class="rounded bg-white px-4 py-2 text-xs font-bold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:cursor-pointer hover:bg-gray-50"
-                      onClick$={() => (deleteMenuOpen.value = false)}
-                    >
-                      No
-                    </button>
-                  </div>
-                </div>
+                <ConfirmDeletion
+                  onYes={$(async () => {
+                    deleteMenuOpen.value = false
+                    await deleter({ id: event.id })
+                    refetcher.value++
+                  })}
+                  onNo={$(() => (deleteMenuOpen.value = false))}
+                />
               </div>
             </div>
           </>
