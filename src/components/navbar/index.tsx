9765991b2d6f6a8efbbe7b724453cd51ae79f813
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-27 17:55:14 +0200

    make nav mobile

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable qwik/jsx-img */
-import { component$, useContext, noSerialize } from '@builder.io/qwik'
+import {
+  component$,
+  useContext,
+  noSerialize,
+  useSignal,
+  useTask$,
+} from '@builder.io/qwik'
 import { Link, useLocation, useNavigate, server$ } from '@builder.io/qwik-city'
 import { signOut } from 'firebase/auth'
 import dayjs from 'dayjs'
@@ -60,6 +66,15 @@ export default component$(() => {
   const navigate = useNavigate()
   const location = useLocation()
   const store = useContext(CTX)
+  const menuOpen = useSignal(false)
+
+  useTask$(async ({ track }) => {
+    track(() => location.url.pathname)
+
+    if (menuOpen.value) {
+      menuOpen.value = false
+    }
+  })
 
   return (
     <nav class="bg-white shadow sticky top-0 z-40 bg-[url(../../../oceanDark.jpg)] border border-blue-800">
@@ -116,7 +131,7 @@ export default component$(() => {
                     : ' border-transparent font-normal '
                 }`}
               >
-                About us
+                About Us
               </Link>
             </div>
           </div>
@@ -275,136 +290,85 @@ export default component$(() => {
             {/* Mobile menu button */}
             <button
               type="button"
-              class="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              class="inline-flex items-center justify-center rounded-md p-2 mr-2 text-white hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick$={() => {
+                menuOpen.value = !menuOpen.value
+              }}
             >
               <span class="sr-only">Open main menu</span>
-              {/*
-          Icon when menu is closed.
-          Menu open: "hidden", Menu closed: "block"
-        */}
-              <svg
-                class="block h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-              {/*
-          Icon when menu is open.
-          Menu open: "block", Menu closed: "hidden"
-        */}
-              <svg
-                class="hidden h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              {menuOpen.value ? (
+                <svg
+                  class="block h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  class="block h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              )}
             </button>
           </div>
         </div>
       </div>
       {/* Mobile menu, show/hide based on menu state. */}
-      <div class="sm:hidden" id="mobile-menu">
-        <div class="space-y-1 pb-3 pt-2">
-          {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-          <a
-            href="#"
-            class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-gray-300 hover:bg-gray-100 hover:text-orange-200"
-          >
-            Team
-          </a>
-          <a
-            href="#"
-            class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-gray-300 hover:bg-gray-100 hover:text-orange-200"
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-gray-300 hover:bg-gray-100 hover:text-orange-200"
-          >
-            Calendar
-          </a>
-        </div>
-        <div class="border-t border-gray-200 pb-3 pt-4">
-          <div class="flex items-center px-4">
-            <div class="flex-shrink-0">
-              <img
-                class="h-10 w-10 rounded-full"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-            </div>
-            <div class="ml-3">
-              <div class="text-base font-medium text-gray-800">Tom Cook</div>
-              <div class="text-sm font-medium text-white">tom@example.com</div>
-            </div>
-            <button
-              type="button"
-              class="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-orange-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      {menuOpen.value && (
+        <div class="sm:hidden" id="mobile-menu">
+          <div class="space-y-1 pb-3 pt-2">
+            {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+            <Link
+              href="/"
+              class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
             >
-              <span class="sr-only">View notifications</span>
-              <svg
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                />
-              </svg>
-            </button>
-          </div>
-          <div class="mt-3 space-y-1">
-            <a
-              href="#"
-              class="block px-4 py-2 text-base font-medium text-white hover:bg-gray-100 hover:text-gray-800"
+              Events
+            </Link>
+            <Link
+              href="/sar"
+              class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-gray-300 hover:bg-gray-100 hover:text-orange-200"
             >
-              Your Profile
-            </a>
-            <a
-              href="#"
-              class="block px-4 py-2 text-base font-medium text-white hover:bg-gray-100 hover:text-gray-800"
+              SAR NGOs
+            </Link>
+            <Link
+              href="/publications"
+              class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-gray-300 hover:bg-gray-100 hover:text-orange-200"
             >
-              Settings
-            </a>
-            <a
-              href="#"
-              class="block px-4 py-2 text-base font-medium text-white hover:bg-gray-100 hover:text-gray-800"
+              Publications
+            </Link>
+            <Link
+              href="/articles"
+              class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-gray-300 hover:bg-gray-100 hover:text-orange-200"
             >
-              Sign out
-            </a>
+              Articles
+            </Link>
+            <Link
+              href="/about"
+              class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-white hover:border-gray-300 hover:bg-gray-100 hover:text-orange-200"
+            >
+              About Us
+            </Link>
           </div>
         </div>
-      </div>
+      )}
     </nav>
   )
 })
