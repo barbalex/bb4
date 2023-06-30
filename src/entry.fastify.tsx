97c30dd1e2f79f8e9da39f597f318d6f59d38aea
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-30 10:54:36 +0200

    listen to 0.0.0.0

--- src/entry.fastify.tsx
+++ src/entry.fastify.tsx
@@ -7,29 +7,29 @@
  * - https://qwik.builder.io/docs/deployments/node/
  *
  */
-import { type PlatformNode } from '@builder.io/qwik-city/middleware/node';
-import Fastify from 'fastify';
-import { join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import FastifyQwik from './plugins/fastify-qwik';
+import { type PlatformNode } from '@builder.io/qwik-city/middleware/node'
+import Fastify from 'fastify'
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import FastifyQwik from './plugins/fastify-qwik'
 
 declare global {
   interface QwikCityPlatform extends PlatformNode {}
 }
 
 // Directories where the static assets are located
-const distDir = join(fileURLToPath(import.meta.url), '..', '..', 'dist');
-const buildDir = join(distDir, 'build');
+const distDir = join(fileURLToPath(import.meta.url), '..', '..', 'dist')
+const buildDir = join(distDir, 'build')
 
 // Allow for dynamic port
-const PORT = parseInt(process.env.PORT ?? '3000');
+const PORT = parseInt(process.env.PORT ?? '3000')
 
 const start = async () => {
   // Create the fastify server
   // https://www.fastify.io/docs/latest/Guides/Getting-Started/
   const fastify = Fastify({
     logger: true,
-  });
+  })
 
   // Enable compression
   // https://github.com/fastify/fastify-compress
@@ -37,10 +37,10 @@ const start = async () => {
   // await fastify.register(import('@fastify/compress'))
 
   // Handle Qwik City using a plugin
-  await fastify.register(FastifyQwik, { distDir, buildDir });
+  await fastify.register(FastifyQwik, { distDir, buildDir })
 
   // Start the fastify server
-  await fastify.listen({ port: PORT });
-};
+  await fastify.listen({ port: PORT, host: '0.0.0.0' })
+}
 
-start();
+start()
