3fcdbcb8fd308b0db628cd30db8a3a6d9dfaeb47
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-30 21:56:15 +0200

    add tag on change tags

--- src/sql/triggers.sql
+++ src/sql/triggers.sql
@@ -10,26 +10,31 @@ BEGIN
     NEW.tags_sort = 1;
     ELSEIF(NEW.tags ? 'statistics') THEN
     NEW.tags_sort = 1;
+    NEW.tag = 'statistics';
   END IF;
   IF(NEW.tag = 'monthlyStatistics') THEN
     NEW.tags_sort = 2;
     ELSEIF(NEW.tags ? 'monthlyStatistics') THEN
     NEW.tags_sort = 2;
+    NEW.tag = 'monthlyStatistics';
   END IF;
   IF(NEW.tag = 'victims') THEN
     NEW.tags_sort = 3;
     ELSEIF(NEW.tags ? 'victims') THEN
     NEW.tags_sort = 3;
+    NEW.tag = 'victims';
   END IF;
   IF(NEW.tag = 'highlighted') THEN
     NEW.tags_sort = 4;
     ELSEIF(NEW.tags ? 'highlighted') THEN
     NEW.tags_sort = 4;
+    NEW.tag = 'highlighted';
   END IF;
   IF(NEW.tag = 'weather') THEN
     NEW.tags_sort = 5;
     ELSEIF(NEW.tags ? 'weather') THEN
     NEW.tags_sort = 5;
+    NEW.tag = 'weather';
   END IF;
   RETURN NEW;
 END;
