e7834ceed0454430388df061645530914f9a3d59
Alexander Gabriel <alex.barbalex@gmail.com>
2023-07-04 23:05:52 +0200

    solve updating issue

--- src/routes/events/[year]/list.tsx
+++ src/routes/events/[year]/list.tsx
@@ -1,4 +1,5 @@
 import { component$ } from '@builder.io/qwik'
+import { useLocation } from '@builder.io/qwik-city'
 
 import EventRow from './eventRow'
 import MonthRow from './monthRow'
@@ -8,25 +9,30 @@ import { useEvents } from '.'
 
 export default component$(() => {
   const events = useEvents()
-  // TODO: data updates, but not the top of the list
+  const location = useLocation()
+  // Problem: data updates, but the top of the list does not rerender correctly
+  // Solved by adding the year to the keys
 
   // DO NOT use a parent div, use a fragment instead
   // div prevents sticky month row
   return events.value.map((row, index) => (
     <>
       {(row.isLastOfMonth || index === 0) && (
-        <MonthRow key={`${row.id}/month`} date={row.date} />
+        <MonthRow
+          key={`${row.id}/month/${location.params.year}`}
+          date={row.date}
+        />
       )}
       {row.isLastOfMonth &&
         (row.migrationStats?.length > 0 || row.politicStats?.length > 0) && (
           <StatisticRow
-            key={`${row.id}/stats`}
+            key={`${row.id}/stats/${location.params.year}`}
             migrationStats={row.migrationStats}
             politicStats={row.politicStats}
           />
         )}
       <EventRow
-        key={`${row.id}/event`}
+        key={`${row.id}/event/${location.params.year}`}
         date={row.date}
         migrationEvents={row.migrationEvents}
         politicEvents={row.politicEvents}
