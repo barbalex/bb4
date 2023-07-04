612948035ccff8eac71d0fca88a89a54a3b8d86a
Alexander Gabriel <alex.barbalex@gmail.com>
2023-07-04 18:28:35 +0200

    get page at /events7

--- src/routes/events/index.tsx
+++ src/routes/events/index.tsx
@@ -1,11 +1,3 @@
-import { component$ } from '@builder.io/qwik'
-import type { RequestHandler } from '@builder.io/qwik-city'
+import Events from '~/routes/'
 
-export const onRequest: RequestHandler = ({ redirect }) => {
-  redirect(308, '/')
-}
-
-// need to export a component due to a bug
-// https://github.com/BuilderIO/qwik/issues/4502
-// TODO: bug fixed 19.6.2023, remove this export when qwik version is updated
-export default component$(() => 'hi')
+export default Events
