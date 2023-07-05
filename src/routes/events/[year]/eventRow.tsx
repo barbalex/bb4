8a36441d26b69c41b2f4d095441c221e5e2375de
Alexander Gabriel <alex.barbalex@gmail.com>
2023-07-05 23:13:25 +0200

    rename

--- src/routes/events/[year]/eventRow.tsx
+++ src/routes/events/[year]/eventRow.tsx
@@ -1,32 +1,30 @@
 import { component$ } from '@builder.io/qwik'
 import dayjs from 'dayjs'
 
-import Event from './listItem'
+import ListItem from './listItem'
 
-export default component$(
-  ({ date, migrationEvents, politicEvents }) => {
-    const day = dayjs(date).format('D')
-    const dayWithEvents = migrationEvents.length > 0 || politicEvents.length > 0
+export default component$(({ date, migrationEvents, politicEvents }) => {
+  const day = dayjs(date).format('D')
+  const dayWithEvents = migrationEvents.length > 0 || politicEvents.length > 0
 
-    return (
-      <div class="border-t border-solid border-slate-200 hover:bg-slate-50 event-row">
-        {dayWithEvents && <p class="p-1.5 pr-5 text-right">{day}</p>}
-        {!dayWithEvents && <p class="p-1.5 pr-5 text-right">{day}</p>}
-        <div class="pr-2.5 break-words">
-          <ul class="px-4">
-            {migrationEvents.map((event) => (
-              <Event key={event.id} event={event} />
-            ))}
-          </ul>
-        </div>
-        <div class="pr-2.5 break-words">
-          <ul class="px-4">
-            {politicEvents.map((event) => (
-              <Event key={event.id} event={event} />
-            ))}
-          </ul>
-        </div>
+  return (
+    <div class="border-t border-solid border-slate-200 hover:bg-slate-50 event-row">
+      {dayWithEvents && <p class="p-1.5 pr-5 text-right">{day}</p>}
+      {!dayWithEvents && <p class="p-1.5 pr-5 text-right">{day}</p>}
+      <div class="pr-2.5 break-words">
+        <ul class="px-4">
+          {migrationEvents.map((event) => (
+            <ListItem key={event.id} event={event} />
+          ))}
+        </ul>
       </div>
-    )
-  },
-)
+      <div class="pr-2.5 break-words">
+        <ul class="px-4">
+          {politicEvents.map((event) => (
+            <ListItem key={event.id} event={event} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+})
