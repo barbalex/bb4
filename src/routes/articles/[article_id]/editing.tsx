372df060ba32e68e5670d7d7012748106fc056c9
Alexander Gabriel <alex.barbalex@gmail.com>
2023-07-05 11:46:22 +0200

    route load article

--- src/routes/articles/[article_id]/editing.tsx
+++ src/routes/articles/[article_id]/editing.tsx
@@ -5,7 +5,7 @@ import {
   useSignal,
   $,
 } from '@builder.io/qwik'
-import { server$ } from '@builder.io/qwik-city'
+import { server$, useNavigate } from '@builder.io/qwik-city'
 import { isServer } from '@builder.io/qwik/build'
 import dayjs from 'dayjs'
 
@@ -54,123 +54,122 @@ const contentUpdater = server$(async function ({ content, id }) {
   return true
 })
 
-export default component$(
-  ({ id, title, datum, content, refetcher: articleRefetcher }) => {
-    const store = useContext(CTX)
+export default component$(({ id, title, datum, content }) => {
+  const store = useContext(CTX)
+  const navigate = useNavigate()
 
-    const dateIsOpen = useSignal(false)
-    const dateElement = useSignal()
-    // useVisibleTask had issues on first render - code did not run reliably
-    useTask$(() => {
-      if (isServer) return
+  const dateIsOpen = useSignal(false)
+  const dateElement = useSignal()
+  // useVisibleTask had issues on first render - code did not run reliably
+  useTask$(() => {
+    if (isServer) return
 
-      // this is essential or the event handler will only run on the first render
-      if (window.editorChangeHandler) return
+    // this is essential or the event handler will only run on the first render
+    if (window.editorChangeHandler) return
 
-      window.editorChangeHandler = async (e) => {
-        await contentUpdater({ id, content: e?.target?.getContent?.() })
-        articleRefetcher.value++
-      }
-    })
+    window.editorChangeHandler = async (e) => {
+      await contentUpdater({ id, content: e?.target?.getContent?.() })
+      navigate()
+    }
+  })
 
-    // https://www.tiny.cloud/docs/tinymce/6/webcomponent-pm/
-    // https://www.tiny.cloud/docs/tinymce/6/webcomponent-ref/
-    return (
-      <>
-        <fieldset class="mt-1 -mr-2.5 mb-2" role="group">
-          <label
-            for="title"
-            class="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            class="block w-full rounded-md border-0 py-1.5 px-3 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            value={title}
-            onChange$={async (e) => {
-              await titleUpdater({ id, title: e?.target?.value })
-              articleRefetcher.value++
-              store.articlesRefetcher++
-            }}
-          />
-        </fieldset>
-        <fieldset
-          class="pb-4 -mr-2.5"
-          onFocusin$={() => {
-            dateIsOpen.value = true
-          }}
-          onFocusout$={() => {
-            dateIsOpen.value = false
-          }}
+  // https://www.tiny.cloud/docs/tinymce/6/webcomponent-pm/
+  // https://www.tiny.cloud/docs/tinymce/6/webcomponent-ref/
+  return (
+    <>
+      <fieldset class="mt-1 -mr-2.5 mb-2" role="group">
+        <label
+          for="title"
+          class="block text-sm font-medium leading-6 text-gray-900"
         >
-          <legend
-            for="title"
-            class="block text-sm font-medium leading-6 col-span-full"
-          >
-            Date
-          </legend>
-          <input
-            ref={dateElement}
-            type="text"
-            name="datum"
-            id="title"
-            class="block w-full rounded-md border-0 py-1.5 px-3 mt-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-            value={datum ? dayjs(datum).format('DD.MM.YYYY') : null}
-            onChange$={async (e, currentTarget) => {
+          Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          class="block w-full rounded-md border-0 py-1.5 px-3 mt-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          value={title}
+          onChange$={async (e) => {
+            await titleUpdater({ id, title: e?.target?.value })
+            navigate()
+            store.articlesRefetcher++
+          }}
+        />
+      </fieldset>
+      <fieldset
+        class="pb-4 -mr-2.5"
+        onFocusin$={() => {
+          dateIsOpen.value = true
+        }}
+        onFocusout$={() => {
+          dateIsOpen.value = false
+        }}
+      >
+        <legend
+          for="title"
+          class="block text-sm font-medium leading-6 col-span-full"
+        >
+          Date
+        </legend>
+        <input
+          ref={dateElement}
+          type="text"
+          name="datum"
+          id="title"
+          class="block w-full rounded-md border-0 py-1.5 px-3 mt-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+          value={datum ? dayjs(datum).format('DD.MM.YYYY') : null}
+          onChange$={async (e, currentTarget) => {
+            await datumUpdater({
+              datum: dateFromInputForDb(currentTarget.value),
+              id,
+            })
+            navigate()
+            store.articlesRefetcher++
+          }}
+          required
+        />
+        <div
+          class={`absolute left-1/2 z-50 flex -translate-x-1/2 px-4 pt-4 mt-1 bg-white ${
+            dateIsOpen.value
+              ? 'transition ease-in opacity-100 translate-y-0'
+              : 'transition duration-200 ease-out opacity-0 translate-y-1 h-0'
+          }`}
+        >
+          <Calendar
+            datum={datum}
+            element={dateElement}
+            updater={$(async (datum) => {
               await datumUpdater({
-                datum: dateFromInputForDb(currentTarget.value),
+                datum,
                 id,
               })
-              articleRefetcher.value++
+              navigate()
               store.articlesRefetcher++
-            }}
-            required
+              dateIsOpen.value = false
+            })}
           />
-          <div
-            class={`absolute left-1/2 z-50 flex -translate-x-1/2 px-4 pt-4 mt-1 bg-white ${
-              dateIsOpen.value
-                ? 'transition ease-in opacity-100 translate-y-0'
-                : 'transition duration-200 ease-out opacity-0 translate-y-1 h-0'
-            }`}
-          >
-            <Calendar
-              datum={datum}
-              element={dateElement}
-              updater={$(async (datum) => {
-                await datumUpdater({
-                  datum,
-                  id,
-                })
-                articleRefetcher.value++
-                store.articlesRefetcher++
-                dateIsOpen.value = false
-              })}
-            />
-          </div>
-        </fieldset>
-        <div class="-mb-2.5 -mr-2.5">
-          <tinymce-editor
-            api-key="58ali3ylgj6fv1zfjv6vdjkkt32yjw36v1iypn95psmae799"
-            height="calc(100vh - 57px)"
-            menubar="edit insert view format table"
-            plugins="advlist autolink lists link image charmap preview anchor
+        </div>
+      </fieldset>
+      <div class="-mb-2.5 -mr-2.5">
+        <tinymce-editor
+          api-key="58ali3ylgj6fv1zfjv6vdjkkt32yjw36v1iypn95psmae799"
+          height="calc(100vh - 57px)"
+          menubar="edit insert view format table"
+          plugins="advlist autolink lists link image charmap preview anchor
                  searchreplace visualblocks code fullscreen
                  insertdatetime media table code help wordcount"
-            toolbar="undo redo | blocks | bold italic underline forecolor backcolor |
+          toolbar="undo redo | blocks | bold italic underline forecolor backcolor |
                  alignleft aligncenter alignright alignjustify |
                  bullist numlist outdent indent | removeformat | link image code | wordcount print fullscreen"
-            browser_spellcheck="true"
-            branding="false" // does not seem to work
-            resize="false"
-            on-Change="editorChangeHandler"
-          >
-            {content}
-          </tinymce-editor>
-        </div>
-      </>
-    )
-  },
-)
+          browser_spellcheck="true"
+          branding="false" // does not seem to work
+          resize="false"
+          on-Change="editorChangeHandler"
+        >
+          {content}
+        </tinymce-editor>
+      </div>
+    </>
+  )
+})
--- src/routes/articles/[article_id]/index.tsx
+++ src/routes/articles/[article_id]/index.tsx
@@ -1,18 +1,13 @@
-import {
-  component$,
-  useResource$,
-  Resource,
-  useContext,
-  useSignal,
-} from '@builder.io/qwik'
-import { useLocation, server$ } from '@builder.io/qwik-city'
+import { component$, useContext } from '@builder.io/qwik'
+import { useLocation, server$, routeLoader$ } from '@builder.io/qwik-city'
 
 import * as db from '~/db'
 import { CTX } from '~/root'
 import Editing from './editing'
 
 // select all articles: id, title, draft
-const dataFetcher = server$(async function (id) {
+export const useArticle = routeLoader$(async function (requestEvent) {
+  const id = requestEvent.params.article_id
   let res
   try {
     res = await db.query(
@@ -45,93 +40,79 @@ const draftSetter = server$(async function ({ value, id }) {
 })
 
 export default component$(() => {
-  const refetcher = useSignal(0)
   const location = useLocation()
   const store = useContext(CTX)
 
-  const data = useResource$(async ({ track }) => {
-    const id = track(() => location.params.article_id)
-    track(() => refetcher.value)
-
-    return await dataFetcher(id)
-  })
+  const article = useArticle()
 
   return (
-    <Resource
-      value={data}
-      onPending={() => <div>Loading...</div>}
-      onRejected={(reason) => <div>Error: {reason}</div>}
-      onResolved={(data) => (
-        <>
-          {!!store.user && (
-            <div class="absolute top-0 right-10">
-              {data.draft ? (
-                <button
-                  type="button"
-                  class="rounded-full shadow-sm hover:ring-1 hover:ring-inset hover:ring-gray-300 p-1 text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-800"
-                  data-title="publish"
-                  aria-label="publish"
-                  onClick$={async () => {
-                    await draftSetter({
-                      value: false,
-                      id: location.params.article_id,
-                    })
-                    refetcher.value++
-                    store.articlesRefetcher++
-                  }}
-                >
-                  <svg class="block h-6 w-6" fill="red" viewBox="0 0 512 512">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"
-                    />
-                  </svg>
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  class="rounded-full shadow-sm hover:ring-1 hover:ring-inset hover:ring-gray-300 p-1 text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-800"
-                  data-title="unpublish"
-                  aria-label="unpublish"
-                  onClick$={async () => {
-                    await draftSetter({
-                      value: true,
-                      id: location.params.article_id,
-                    })
-                    refetcher.value++
-                    store.articlesRefetcher++
-                  }}
-                >
-                  <svg class="block h-6 w-6" fill="green" viewBox="0 0 448 512">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m427.63 91.441c12.5 12.5 23.108 46.41 10.968 59.259l-239.05 253.01c-19.423 20.557-61.405 18.876-79.201-0.99708l-111.05-124.01c-11.793-13.169-4.5234-54.736 7.9766-67.236s48.753-15.491 61.253-2.9912l85.957 92.338 205.98-217.85c17.089-9.7881 45.464-2.6082 57.165 8.4752z"
-                    />
-                  </svg>
-                </button>
-              )}
-            </div>
-          )}
-          {store.editing ? (
-            <Editing
-              id={location.params.article_id}
-              title={data.title}
-              datum={data.datum}
-              content={data.content}
-              refetcher={refetcher}
-            />
+    <>
+      {!!store.user && (
+        <div class="absolute top-0 right-10">
+          {article.value.draft ? (
+            <button
+              type="button"
+              class="rounded-full shadow-sm hover:ring-1 hover:ring-inset hover:ring-gray-300 p-1 text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-800"
+              data-title="publish"
+              aria-label="publish"
+              onClick$={async () => {
+                await draftSetter({
+                  value: false,
+                  id: location.params.article_id,
+                })
+                navigate()
+                store.articlesRefetcher++
+              }}
+            >
+              <svg class="block h-6 w-6" fill="red" viewBox="0 0 512 512">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"
+                />
+              </svg>
+            </button>
           ) : (
-            <div
-              class="articles"
-              dangerouslySetInnerHTML={
-                data.content ?? '(no content has been created yet)'
-              }
-            />
+            <button
+              type="button"
+              class="rounded-full shadow-sm hover:ring-1 hover:ring-inset hover:ring-gray-300 p-1 text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-800"
+              data-title="unpublish"
+              aria-label="unpublish"
+              onClick$={async () => {
+                await draftSetter({
+                  value: true,
+                  id: location.params.article_id,
+                })
+                navigate()
+                store.articlesRefetcher++
+              }}
+            >
+              <svg class="block h-6 w-6" fill="green" viewBox="0 0 448 512">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m427.63 91.441c12.5 12.5 23.108 46.41 10.968 59.259l-239.05 253.01c-19.423 20.557-61.405 18.876-79.201-0.99708l-111.05-124.01c-11.793-13.169-4.5234-54.736 7.9766-67.236s48.753-15.491 61.253-2.9912l85.957 92.338 205.98-217.85c17.089-9.7881 45.464-2.6082 57.165 8.4752z"
+                />
+              </svg>
+            </button>
           )}
-        </>
+        </div>
       )}
-    />
+      {store.editing ? (
+        <Editing
+          id={location.params.article_id}
+          title={article.value.title}
+          datum={article.value.datum}
+          content={article.value.content}
+        />
+      ) : (
+        <div
+          class="articles"
+          dangerouslySetInnerHTML={
+            article.value.content ?? '(no content has been created yet)'
+          }
+        />
+      )}
+    </>
   )
 })
